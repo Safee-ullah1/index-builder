15ac436a16ef1f59d11326c7845107f406130c2d
Safee-ullah1 <70797527+Safee-ullah1@users.noreply.github.com>
2023-09-14 23:39:35 +0500

    resolved disambiguation error

--- file_creator.py
+++ file_creator.py
@@ -7,12 +7,16 @@ import random
 def create_files(count, output):
     for i in range(count):
         title = wikipedia.random(1)
-        try:
-            p = wikipedia.page(title)
-        except wikipedia.DisambiguationError as e:
-            random_page = random.choice(e.options)
-            text = wikipedia.page(random_page).content
+        is_error = True
+        page = ""
+        while is_error:
+            try:
+                page = wikipedia.page(
+                    title, auto_suggest=False, redirect=True, preload=False)
+                is_error = False
+            except wikipedia.DisambiguationError as e:
+                continue
             if not os.path.isdir(output):
                 os.mkdir(output)
             with open(os.path.join(output, f"{title}.txt"), "w+", encoding="utf-8") as file:
-                file.write(text)
+                file.write(page)
