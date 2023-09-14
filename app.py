99a5b936abb082fa3a0a6795a777440ec514bbe3
Safee-ullah1 <70797527+Safee-ullah1@users.noreply.github.com>
2023-09-14 23:31:37 +0500

    added indexes an searching

--- app.py
+++ app.py
@@ -1,7 +1,34 @@
 import streamlit as st
 import os
 from file_creator import create_files
+from indexer import create_index
+
+# create the index
+files = []
+index = {}
+def on_create_index():
+    if os.path.isdir("files"):
+        files = os.listdir("files")
+        index = create_index("files", "index.json")
+st.button("Create Index", on_click=on_create_index)
+
+# search for words
+search = st.text_input("Search")
+if search:
+    for word in search.split(" "):
+        if word in index:
+            files_info = index[word]
+            file_with_count = {}
+            for file_info in files_info:
+                if file_info not in file_with_count:
+                    file_with_count[file_info] = 0
+                file_with_count[file_info] += 1
+            for file, count in file_with_count.items():
+                st.write(f"{file} - {count}")
+
+
+
 st.button("Add Files", on_click=create_files, args=(10, "files"))
 # display the files
-files = os.listdir("files")
+
 st.write(files)
---
+++ indexer.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+def create_index(directory, output):
+    if not os.path.isdir(directory):
+        raise Exception("Directory not found.")
+    
+    files = os.listdir(directory)
+    index = {}
+    for file in files:
+        full_path = os.path.join(directory, file)
+        if not os.path.isfile(full_path):
+            continue
+        with open(full_path, "r", encoding="utf-8") as input_file:
+            file_text = input_file.read()
+            words = file_text.split(" ")
+            for word in words:
+                if word not in index:
+                    index[word] = []
+                index[word].append(file)
+                
+    with open(output, "w+", encoding="utf-8") as output_file:
+        output_file.write(json.dumps(index, sort_keys=True, indent=4))
+    return index
